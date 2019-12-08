2b032d3571b8f41e5fc74f19cdb481bdc2a2dc70
joshuabeaver_ <47809705+imnotbeavs@users.noreply.github.com>
2019-12-08 18:26:31 +0800

    Update matlab_MP_4.m

--- matlab_MP_4.m
+++ matlab_MP_4.m
@@ -15,22 +15,31 @@ elseif height<0
     error('Initial height nust be from above ground')
 end
 
-time = ((velocity*sind(theta)) + sqrt((velocity*sind(theta))^2 + 2*abs(y_acc)*height))/abs(y_acc);
+time = ((velocity*sind(theta)) + sqrt((velocity*sind(theta))^2 + 2*abs(y_acc)*height))/abs(y_acc)
 t_vector = linspace(0,time,1000);
+    %--ideal time for the trajectory to hit the ground(gravity = -9.8)---%
+ideal_time = ((velocity*sind(theta)) + sqrt((velocity*sind(theta))^2 + 2*abs(-9.8)*height))/abs(-9.8);
+ideal_tvector = linspace(0,ideal_time,1000);
 
 horizontal_eqn = [(x_acc)/2, velocity*cosd(theta), 0];
-vertical_eqn = [(y_acc)/2, velocity*sind(theta), height];
+vertical_eqn = [(y_acc)/2, velocity*sind(theta), height];                
+    %--ideal trajectory of the projectile(the only acceleration is gravity = -9.8)--%
+ideal_h_eqn = [velocity*cosd(theta),0];
+ideal_v_eqn = [-4.9,velocity*sind(theta),height];
 
 h_vector = polyval(horizontal_eqn,t_vector);
 v_vector = polyval(vertical_eqn, t_vector);
-
+    %--vector for the ideal trajectory of the projectile----------------%
+ideal_h_vector = polyval(ideal_h_eqn,ideal_tvector);        
+ideal_v_vector = polyval(ideal_v_eqn,ideal_tvector);
 
 plot(h_vector,v_vector,'linewidth',2.2)
-grid on
+hold on 
+plot(ideal_h_vector,ideal_v_vector,'linewidth',2.2)
+grid 
+legend('Non-ideal Trajectory','Ideal Trajectory')
 xlabel('Horizontal Displacement')
 ylabel('Vertical Displacement')
 title('Projectile Trajectory')
 
 end
-
-
